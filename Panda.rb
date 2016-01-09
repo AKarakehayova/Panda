69e32146e6480a75fbb089107d12ed5d2ea34c9b
AKarakehayova <alexzk95@gmail.com>
2016-01-09 11:26:20 +0200

    Update Panda.rb

--- Panda.rb
+++ Panda.rb
@@ -1,1 +1,68 @@
+class Panda
+  attr_accessor :name, :email, :gender, :friends
+  def initialize(name, email, gender)
+    @name = name
+    @email = email
+    @gender = gender
+    @friends = []
+  end
 
+  def male?
+    if gender == "male"
+      true
+    else false
+    end
+  end
+
+  def female?
+    if gender == "female"
+      true
+    else false
+    end
+  end
+
+  def to_s(panda)
+    "#{panda.name}, #{panda.email}, #{panda.gender}"
+  end
+
+  def ==(panda)
+    name == panda.name && email == panda.email && gender == panda.gender
+  end
+
+  def hash
+    panda.email.hash
+  end
+end
+
+class SocialNetwork
+  attr_accessor :members
+  def initialize
+    @members = {}
+  end
+
+  def add_panda(panda)
+    if members.include? panda
+      raise PandaAlreadyThere, "This panda already exists in the network."
+    else members[panda] = panda.friends
+    end
+  end
+
+  def has_panda(panda)
+    members.key? panda
+  end
+
+  def make_friends(panda1, panda2)
+    if !has_panda panda1
+      add_panda panda1
+    elsif !had_panda panda2
+      add_panda panda2
+    end
+
+    if panda1.friends.include? panda2
+      raise PandasAlreadyFriends, "These pandas are already friends"
+    else
+    panda1.friends.push(panda2)
+    panda2.friends.push(panda1)
+    end
+  end
+end
